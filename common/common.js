e2d2452bb8c3db488d9ce7af10a279ef291980e6
dl77sea <danlarsson77@gmail.com>
2018-02-06 15:16:30 -0800

    ok

--- common/common.js
+++ common/common.js
@@ -24,7 +24,7 @@ function Common(contentGraphService) {
 
     ctrl.defaultBfw = 30
     ctrl.defaultDesignLifetime = 2050
-    ctrl.defaultBfwDesign = ctrl.defaultBfw
+    ctrl.defaultBfwDesign = 34
 
     //toolbar, graph, will need lat and lon from map click from mapService
     ctrl.coords = {lat: ctrl.defaultLat, lng: ctrl.defaultLng}
