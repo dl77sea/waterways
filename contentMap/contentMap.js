16f6ffb88b28742f43b3cb7548aa36a84126d731
dl77sea <danlarsson77@gmail.com>
2018-02-23 11:18:10 -0800

    ok

--- contentMap/contentMap.js
+++ contentMap/contentMap.js
@@ -22,7 +22,7 @@
     commonService.editMode.mode = "map"
 
     ctrl.defaultLat = 45.65625
-    ctrl.defaultLng = -121.09375 
+    ctrl.defaultLng = -121.09375
     ctrl.coords = {
       lat: ctrl.defaultLat,
       lng: ctrl.defaultLng
@@ -38,7 +38,7 @@
           lat: ctrl.coords.lat,
           lng: ctrl.coords.lng
         },
-        zoom: 11,
+        zoom: 12,
         styles: [{
             "elementType": "geometry",
             "stylers": [{
