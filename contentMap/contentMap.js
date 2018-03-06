6684fe6f6c30be986fa743ba037a8f49b764664f
dl77sea <danlarsson77@gmail.com>
2018-03-06 14:21:15 -0800

    ok

--- contentMap/contentMap.js
+++ contentMap/contentMap.js
@@ -317,10 +317,6 @@
           ctrl.selectedLat = this.getBounds().getCenter().lng()
 
           commonService.editMode.mode = "graph"
-          
-          //set this for ratios file download
-          // commonService.coords.lat = commonService.selectedTile.getBounds().getCenter().lat()
-          // commonService.coords.lng = commonService.selectedTile.getBounds().getCenter().lng()
 
           //go to graph component
           $state.go('common-top.content-graph', {
